090c739875cc574e9eab1a8deeb5100b72d30cb7
Velayutham <velu.18.8.2004@gmai.com>
2025-06-24 01:07:50 +0530

    Initial commit

---
+++ src/main/java/com/smartnotes/SmartNotesApplication.java
@@ -0,0 +1,11 @@
+package com.smartnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartNotesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartNotesApplication.class, args);
+    }
+}
